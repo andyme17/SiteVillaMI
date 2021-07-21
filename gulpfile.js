0e92ba0ec70dc82d0806d1811002b91a90c3ad6a
ZunoRick <zunosan.ricardo506@gmail.com>
2021-07-20 22:59:48 -0500

    chore: add imagemin and javascript settings in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -16,10 +16,27 @@ function compileSASS() {
   return src(PATHS.scss).pipe(sass()).pipe(dest('./public/build/assets/css'));
 }
 
+function javascript() {
+  return src(PATHS.js).pipe(dest('./public/build/assets/js'));
+}
+
+function imagenes() {
+  return src(PATHS.img)
+    .pipe(cache(imagemin({ optimizationLevel: 3 })))
+    .pipe(dest('./public/build/img'));
+}
+
+function versionWebp() {
+  return src(PATHS.img).pipe(webp()).pipe(dest('./public/build/img'));
+}
+
 function watchFiles() {
   watch(PATHS.scss, compileSASS);
+  watch(PATHS.js, javascript);
+  watch(PATHS.img, imagenes);
+  watch(PATHS.img, versionWebp);
 }
 
 exports.compileSASS = compileSASS;
 
-exports.default = parallel(watchFiles);
+exports.default = parallel(javascript, imagenes, versionWebp, watchFiles);
