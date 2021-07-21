e2a7d7ed93084820762009aefae694b9c3f98cd3
andyme17 <ame171337@gmail.com>
2021-07-20 22:27:15 -0500

    chore: create start gulp settings in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 'use strict';
 
-const { src, dest } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const cache = require('gulp-cache');
+const webp = require('gulp-webp');
+const imagemin = require('gulp-imagemin');
+
+const PATHS = {
+  scss: 'src/assets/scss/**/*.scss',
+  js: 'src/assets/js/**/*.js',
+  img: 'src/assets/img/**/*',
+};
 
 function compileSASS() {
-  return src('./src/assets/scss/**/*.scss')
-    .pipe(sass())
-    .pipe(dest('./build/assets/css'));
+  return src(PATHS.scss).pipe(sass()).pipe(dest('./public/build/assets/css'));
+}
+
+function watchFiles() {
+  watch(PATHS.scss, compileSASS);
 }
 
 exports.compileSASS = compileSASS;
+
+exports.default = parallel(watchFiles);
